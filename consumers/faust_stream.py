929e8a0dae5919dc55b140aa2052d8ed930d1c14
David Newton <davidwillnewton@gmail.com>
2022-04-18 15:13:18 +1200

    Add faust application

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -1,11 +1,8 @@
 """Defines trends calculations for stations"""
 import logging
-
 import faust
 
-
 logger = logging.getLogger(__name__)
-
 
 # Faust will ingest records from Kafka in this format
 class Station(faust.Record):
@@ -20,7 +17,6 @@ class Station(faust.Record):
     blue: bool
     green: bool
 
-
 # Faust will produce records to Kafka in this format
 class TransformedStation(faust.Record):
     station_id: int
@@ -28,31 +24,42 @@ class TransformedStation(faust.Record):
     order: int
     line: str
 
-
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
+# Faust Stream that ingests data from the Kafka Connect stations topic and
+# places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+# Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
+topic = app.topic("postgresstations", value_type=Station)
+# Define the output Kafka Topic
+out_topic = app.topic("com.udacity.cta.stations.transformed", value_type=TransformedStation, partitions=1)
+# Define a Faust Table
+table = app.Table(
+   "transformed_station_data",
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
-
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
+# Using Faust, transform input `Station` records into `TransformedStation` records. Note that
 # "line" is the color of the station. So if the `Station` record has the field `red` set to true,
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def transform(station):
+    async for s in station:
+        if s.red:
+            line = "red"
+        elif s.blue:
+            line = "blue"
+        else:
+            line = "green"
 
+        transformed_station = TransformedStation(
+            station_id = s.station_id,
+            station_name = s.station_name,
+            order = s.order,
+            line = line
+        )
+
+        table[s.station_id] = transformed_station
 
 if __name__ == "__main__":
     app.main()
