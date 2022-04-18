d5bc68d4e87011f7e6dc66e05a35b7028feeb344
David Newton <davidwillnewton@gmail.com>
2022-04-18 13:26:35 +1200

    Add turnstile class

---
+++ producers/models/turnstile.py
@@ -0,0 +1,60 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+import time
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+logger = logging.getLogger(__name__)
+
+#########################################################
+# This, along with Turnstile hardware, simulates riders #
+# entering the station                                  #
+#########################################################
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        super().__init__(
+            "com.udacity.cta.turnstile.events",
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema,
+            num_partitions=3,
+            num_replicas=2,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+        for _ in range(num_entries):
+            time.sleep(0.5)
+            logger.info(
+                "Rider %s out of %s entering %s",
+                _,
+                num_entries,
+                self.station
+            )
+            self.producer.produce(
+                topic = self.topic_name,
+                key = {"timestamp": self.time_millis()},
+                value = {
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.name,
+                    "line": self.station.color.name,
+                },
+            )
